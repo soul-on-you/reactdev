bc882c3e5fa393de7950ab03322e7eb8420f8d5d
brendan <superden081@gmail.com>
2022-05-27 20:00:22 +0300

    graphql-intro how to work backend side

---
+++ react_graphql/server/index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const cors = require("cors");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./graphql/index");
+const { nanoid } = require("nanoid");
+const users = [{ id: 1, username: "Vova", age: 212 }];
+
+const app = express();
+app.use(cors());
+
+const getAllUsers = () => users;
+
+const createUser = (input) => {
+  return { id: nanoid(10), ...input };
+};
+
+const resolver = {
+  getAllUsers, // аналогичная запись getAllUsers: () => {return users},
+  getUser: ({ id }) => {
+    return users.find((user) => user.id == id);
+  },
+  createUser: ({ input }) => {
+    const user = createUser(input);
+    users.push(user);
+    return user;
+  },
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({ graphiql: true, schema, rootValue: resolver })
+);
+
+const port = 3666;
+app.listen(port, console.log(`Сервер запущен на ${port} порту`));
+
+/**
+ * Мутация данных, в примере, добовление пользователя
+ * mutation{
+    createUser(input: {
+        username: "VI"
+        age: 222
+    }) {
+        id, username
+    }
+}
+ * Запрос очереди на получение данных
+ * query{
+    getAllUsers{
+      username, id, age
+    }
+    getUser(id:1){
+      username, age
+    }
+  }
+ * Создаем пользователя с постами
+ * mutation {
+    createUser(input: {username: "VVV", age: 166, posts: [{id: 1, title: "first message", content: "hello graphql"}]}) {
+      id
+    }
+  }
+ *Получаем пользователей и их посты
+ * query{
+    getAllUsers{
+      id, username, posts {
+        id, title, content
+      }
+    }
+  }
+*/
