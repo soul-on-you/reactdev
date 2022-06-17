94f17005d5bccfe85add9de71010f29810e1bb0b
brendan <superden081@gmail.com>
2022-06-17 19:06:16 +0300

    update App component, connected login and auth store state updated work, privete content and public routes managed

--- react_redux_jwt_auth/client/src/App.js
+++ react_redux_jwt_auth/client/src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
-import { Counter } from "./features/counter/Counter";
-import "./App.css";
+import Counter from "./pages/counter/Counter.jsx";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout.jsx";
+import Home from "./pages/home/Home.jsx";
+import Page404 from "./pages/page404/Page404.jsx";
+import Login from "./pages/login/Login.jsx";
+import RequireAuth from "./components/auth/RequireAuth.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <Counter />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+
+        {/* private routes */}
+        <Route element={<RequireAuth />}>
+          <Route path="counter" element={<Counter />} />
+        </Route>
+
+        {/* error route */}
+        <Route path="*" element={<Page404 />} />
+      </Route>
+    </Routes>
   );
 }
 
--- react_redux_jwt_auth/client/src/index.js
+++ react_redux_jwt_auth/client/src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import "./index.css"
 import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -10,7 +12,11 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
