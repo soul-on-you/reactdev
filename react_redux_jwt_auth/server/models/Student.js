e3522e4bcbf1e6553ad8c6618cd2ecc839d269f8
brendan <superden081@gmail.com>
2022-07-01 13:36:56 +0300

    fix models

--- react_redux_jwt_auth/server/models/Student.js
+++ react_redux_jwt_auth/server/models/Student.js
@@ -2,8 +2,19 @@ const { Schema, model, ObjectId } = require("mongoose");
 
 const Student = new Schema({
   userId: { type: ObjectId, unique: true, required: true, ref: "User" }, //!
-  tasks: [{ type: ObjectId, ref: "Task" }],
-  passed: {type: Boolean, required: true, default: false},
+  tasks: [
+    // { type: ObjectId, ref: "Task" },
+    {
+      detailId: { type: ObjectId, required: true, ref: "Detail3D" }, //!
+      passed: { type: Boolean, required: true, default: false },
+      moderation: { type: Boolean, required: true, default: false },
+      mark: { type: Number, required: false },
+      comment: { type: String, required: false },
+      createdAt: { type: Date, default: Date.now },
+      updatedAt: { type: Date, default: Date.now },
+    },
+  ],
+  passed: { type: Boolean, required: true, default: false },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
--- react_redux_jwt_auth/server/models/User.js
+++ react_redux_jwt_auth/server/models/User.js
@@ -4,7 +4,12 @@ const User = new Schema({
   serialNumber: { type: Number, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: ObjectId, default: "62a1df56d15e9a4caf9cb1c6", ref: "Role" },
+  // role: { type: ObjectId, default: "62a1df56d15e9a4caf9cb1c6", ref: "Role" },
+  role: {
+    type: String,
+    enum: ["student", "professor", "admin"],
+    default: "student",
+  },
   avatar: { type: String },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
