db5da5fac83bdbb45d782aedf2fa5fd74d3f15fd
brendan <superden081@gmail.com>
2022-07-01 13:36:25 +0300

    dev project when controll routes on client side

---
+++ react_router_role_protection/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { AuthProvider } from "./context/AuthProvider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
